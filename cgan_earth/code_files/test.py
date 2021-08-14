33bdab3b4049eea4a8a8952b6a8ca3398092de98
katiezzzzz <katiezeng2000@gmail.com>
2021-08-14 15:34:20 +0800

    sync training images changes

--- cgan_earth/code_files/test.py
+++ cgan_earth/code_files/test.py
@@ -1,15 +1,16 @@
 import numpy as np
 import torch
+import torch.nn.functional as F
 
-array1 = np.array([[[1, 1], [1, 1], [1, 1]],
-                  [[2, 2], [2, 2], [2, 2]],
-                  [[3, 3], [3, 3], [3, 3]]])
-array2 = np.array([1, 2, 3])
-
-shuffler = np.random.permutation(len(array1))
-array1_shuffled = array1[shuffler]
-array2_shuffled = array2[shuffler]
-
-a = torch.zeros((20,5))
-a = a.repeat(64, 64)
-print(a.shape)
+a = torch.tensor([[[1, 2, 3, 4],
+                  [4, 5, 6, 7],
+                  [5, 6, 7, 8],
+                  [8, 9, 10, 11]],
+                 [[11, 21, 31, 41],
+                  [41, 51, 61, 71],
+                  [51, 61, 71, 81],
+                  [81, 91, 101, 111]]])
+p1d = (2,2)
+out = F.pad(a, p1d, "circular")
+print(a)
+print(out)
